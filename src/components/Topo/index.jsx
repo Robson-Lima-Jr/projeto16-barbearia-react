44a74f3241438d4c562971c37a8d334c92e7af68
Robson-Jr <robsonjuniorsn@gmail.com>
2024-07-19 19:50:54 -0300

    topo, imagem e botao criados

--- src/components/Topo/index.jsx
+++ src/components/Topo/index.jsx
@@ -1,4 +1,7 @@
+// imports globais
+"use client";
 import styles from "./Topo.module.css"
+import { useState } from "react";
 import Image from "next/image";
 
 // import icones
@@ -8,12 +11,18 @@ import { BsSun, BsMoon } from "react-icons/bs";
 import Logo from "../../../public/barbearia.png";
 
 export default function Topo() {
+    const [fundoTela, setFundoTela] = useState (false);
+
+    function mudarTela () {
+        setFundoTela(!fundoTela);
+    }
+
     return (
         <header className={styles.topo_area}>
             <div className={styles.container_base}>
                 <Image src={Logo} alt="logo barbearia" className={styles.img_logo}></Image>
 
-                <button className={styles.btn_config}>click</button>
+                <button onClick={mudarTela} className={styles.btn_config}>{fundoTela ?  <BsSun /> : <BsMoon />}</button>
             </div>
         </header>
     );
